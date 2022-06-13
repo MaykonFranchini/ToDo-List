dcba7eda026ebc33e16b90e5452168aa4f0a849b
MaykonFranchini <maykonfranchini2017@gmail.com>
2022-06-13 10:57:39 +0100

    Header componente created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-
+import { Header } from './components/Header'
+import  './global.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <h1>ToDo List</h1>
+    <Header />
   )
 }
 
